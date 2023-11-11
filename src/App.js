08519aa68dac5192909040395e6cf508db49f198
braa <Braashaban101@gmail.com>
2023-11-11 22:19:27 +0300

    first braa

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import Calc from './Components/Calc/Calc';
+
+function App() {
+
+  return (
+    
+    <div className="App">
+ <Calc/>
+    </div>
+  );
+}
+
+export default App;
